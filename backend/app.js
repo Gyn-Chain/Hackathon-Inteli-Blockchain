6d9e1e1fbaa8534ee489c74fd3cf9de6562c2a7a
MarceloAssis123 <124265415+MarceloAssis123@users.noreply.github.com>
2023-05-10 12:15:10 -0300

    Feat: Transaction with ETH

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,6 @@
-const express = require('express'); 
+//const contract = require("@openzeppelin/contracts/token/ERC20/IERC20.sol");
+const express = require('express');
+const mime = require('mime');
 const bodyParser = require('body-parser');
 const urlencodedParser = bodyParser.urlencoded({ extended: false })
 
@@ -8,17 +10,8 @@ const app = express();
 
 /* Colocar toda a parte estática no frontend */
 app.use(express.static("../frontend/html/"));
-app.use(express.static("../frontend/css/"));
 
 
-
-function metamesk() {
-    window.ethereum.request({ method: 'eth_accounts' })
-    .then(console.log(accounts[0]))
-    .catch((err) => {
-      console.error(err);
-    });
-}
 
 
 app.listen(port, hostname, () => {
